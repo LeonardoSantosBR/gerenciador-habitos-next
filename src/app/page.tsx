3b9fdfb71c886779115eeabf2b840f09a688a5c6
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-07-09 14:26:04 -0300

    feat: text add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,14 @@
-export default function Home() {}
+export default function Home() {
+  const habits = {};
+
+  return (
+    <main className="container relative flex flex-col gap-8 px-4 pt-16">
+      {habits === null ||
+        (Object.keys(habits).length === 0 && (
+          <h1 className="mt-20 text-4xl text-white font-light font-display text-center">
+            Você não tem hábitos cadastrados
+          </h1>
+        ))}
+    </main>
+  );
+}
